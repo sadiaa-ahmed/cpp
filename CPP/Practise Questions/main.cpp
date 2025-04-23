1fa58c44d04c5254d80d9e5bbf844d4c883c8223
Sadia Ahmed <148627961+sadiaa-ahmed@users.noreply.github.com>
2025-04-23 02:12:39 -0700

    Add files via upload

---
+++ CPP/Practise Questions/main.cpp
@@ -0,0 +1,193 @@
+//Problems 1
+//#include <iostream>
+//#include <conio.h> // Required for getch()
+//
+//using namespace std;
+//
+//main()
+//{
+//    int a;
+//    cout << "Please Enter 4 Digit Number: ";
+//    cin >> a;
+//
+//    cout << "\n 4 Digits are: ";
+//    cout << a % 10 << ", ";
+//    a = a / 10;
+//    cout << a % 10 << ", ";
+//    a = a / 10;
+//    cout << a % 10 << ", ";
+//    a = a / 10;
+//    cout << a % 10;
+//
+//    getch(); 
+//    return 0;
+//}
+
+//Problem 02 
+
+//#include <iostream>
+//#include <conio.h> 
+//
+//using namespace std;
+//
+//main()
+//{
+//	Airthmetic Operators  + = - /
+//  Assignment Operators  = += =+ /= %=
+//	Comparision Operators  == > < <= >= != 
+//  Logical AND OR NOT (&& || ! )
+
+//	int amir_age , ammara_age;
+//	cout << "\n Enter age of Amir: ";
+//    cin >> amir_age;
+//    cout << "\n Enter age of Ammara: ";
+//    cin >> ammara_age;
+//	if (amir_age > ammara_age)
+//	{
+//		cout<<"\n\n Amir is older than Ammara";
+//	}else{
+//		cout<<"\n\n Ammara is older than Amir";
+//	}
+//	getch();
+//	return 0 ;
+//}
+// Problem 03 
+//#include <iostream>
+//#include <conio.h> 
+//
+//using namespace std;
+//
+//main()
+//{
+//	int start = 1 ;
+//	while(start<= 10)
+//	{
+//		cout<<"\n\n Hey";
+//		start++ ;
+//	}
+////	int i ;
+////	for(i = 0 ; i < 3 ; i++){
+////		cout<< "\n And He Found You Lost And Guided You";
+////	}
+//	getch();
+//	return 0 ;
+//}
+
+//Problems: 04  Lecture 06
+//#include <iostream>
+//#include <conio.h> // Required for getch()
+//
+//using namespace std;
+//
+//main()
+//{
+//	int start = 1 , sum =0 ; 
+//	while (start <= 100)
+//	{
+//		sum += start ; 
+//		start ++ ; 
+//	}
+//	cout<< "\n sum is :"<<sum ;
+//	getch();
+//	return 0;
+//}
+//***********************************************************************
+// Problem 05  Lecture 07 
+
+//#include <iostream>
+//#include <conio.h> // Required for getch()
+//
+//using namespace std;
+//main()
+//{
+//	// while loop    ?   Runs continuously until the condition becomes false
+//    // do-while loop ?   Runs at least once and then depends on the user's input or condition
+//    // for loop      ?   Used when the number of repetitions is known (limited repetition)
+//    
+//    int start = 1 ; 
+//    do
+//    {
+//    	cout<< "\n Virtual University";
+//    	start++ ; 
+//	}while(start <= 5);
+//	
+//	getch();
+//	return 0 ;    
+//}
+//  
+
+//Problem 06 
+//#include <iostream>
+//#include <conio.h> // Required for getch()
+//
+//using namespace std;
+//main()
+//{
+//	// while loop    ?   Runs continuously until the condition becomes false
+//    // do-while loop ?   Runs at least once and then depends on the user's input or condition
+//    // for loop      ?   Used when the number of repetitions is known (limited repetition)
+//    
+//    char choice ; 
+//    do
+//    {
+//    	cout<< "\n Virtual University";
+//    	cout<< "\n Do You Want to Continue (Y , N) : ";
+//    	cin>>choice; 
+//	}while(choice == 'Y' || choice == 'y');
+//	
+//	getch();
+//	return 0 ;    
+//}
+//  
+// Problem 07 
+ 
+//#include <iostream>
+//#include <conio.h> // Required for getch()
+//
+//using namespace std;
+//
+//main()
+//{
+//	for(int i = 1 ; i <= 5 ; i++ )
+//	{
+//		cout<< "\n\n Man Antaa? ";
+//	}
+//	getch();
+//	return 0 ;
+//}
+
+////Problem 08
+//#include <iostream>
+//#include <conio.h> // Required for getch()
+//
+//using namespace std;
+//
+//main()
+//{
+//	for(int i = 1 ; i <= 5 ; i++ )
+//	{
+//		cout<< "\n\n Man Antaa? ";
+//	}
+//	getch();
+//	return 0 ;
+//}
+
+
+//Problem 09
+// #include <iostream>
+// #include <conio.h> // Required for getch()
+
+// using namespace std;
+
+// main()
+// {
+// 	for(int i = 1 ; i <= 10 ; i++ )
+// 	{
+// 		cout<< "\n 5 * "<<i<<"=" << 5*i;
+// 	}
+// 	getch();
+// 	return 0 ;
+// }
+
+
+
